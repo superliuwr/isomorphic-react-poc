84b3df52d8adfc96f30e50cd8cd7ccec6d32f74f
Marvin Liu <superliuwr@gmail.com>
2017-05-24 22:48:37 +1000

    Add routing

--- browser.js
+++ browser.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
-import ContactsApp from './app/components/ContactsApp';
+import { Router } from 'react-router';
+import { createHistory } from 'history';
+import routes from './app/routes';
 
-let initialData = document.getElementById('initial-data').textContent;
-
-if(initialData.length>0){
-    initialData = JSON.parse(initialData);
+let handleCreateElement = (Component, props) => {
+    if (Component.hasOwnProperty('requestInitialData')) {
+        let initialData = document.getElementById('initial-data').textContent;
+        if (initialData.length > 0) {
+            initialData = JSON.parse(initialData);
+        }
+        return <Component initialData={initialData} {...props} />;
+    } else {
+        return <Component {...props} />;
+    }
 }
 
-render(<ContactsApp initialData={initialData} />, document.getElementById('root'));
+render((<Router history={createHistory()} createElement={handleCreateElement}>{routes}</Router>),
+    document.getElementById('root'))
--- server.js
+++ server.js
@@ -2,26 +2,64 @@ import fs from 'fs';
 import express from 'express';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
-import ContactsApp from './app/components/ContactsApp';
+import { matchPath, RoutingContext } from 'react-router';
+import routes from './app/routes';
 
 const app = express();
 app.set('views', './')
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
-
 const contacts = JSON.parse(fs.readFileSync(__dirname + '/public/contacts.json', 'utf8'));
 
-const ContactsAppFactory = React.createFactory(ContactsApp);
+let getPropsFromRoute = ({ routes }, componentProps) => {
+	let props = {};
+	let lastRoute = routes[routes.length - 1];
+	routes.reduceRight((prevRoute, currRoute) => {
+		componentProps.forEach(componentProp => {
+			if (!props[componentProp] && currRoute.component[componentProp]) {
+				props[componentProp] = currRoute.component[componentProp];
+			}
+		});
+	}, lastRoute);
+	return props;
+};
 
-app.get('/', (request, response) => {
-	let componentInstance = ContactsAppFactory({initialData:contacts});
-	
-	response.render('index',{
-		reactInitialData: JSON.stringify(contacts),
-		content: renderToString(componentInstance)
+let renderRoute = (response, renderProps) => {
+	let routeProps = getPropsFromRoute(renderProps, ['requestInitialData']);
+	if (routeProps.requestInitialData) {
+		routeProps.requestInitialData().then((data) => {
+			let handleCreateElement = (Component, props) => (
+				<Component initialData={data} {...props} />
+			);
+			response.render('index', {
+				reactInitialData: JSON.stringify(data),
+				content: renderToString(
+					<RoutingContext createElement={handleCreateElement} {...renderProps} />
+				)
+			});
+		});
+	} else {
+		response.render('index', {
+			reactInitialData: null,
+			content: renderToString(<RoutingContext {...renderProps} />)
+		});
+	}
+};
+
+app.get('*', (request, response) => {
+	matchPath({ routes, location: request.url }, (error, redirectLocation, renderProps) => {
+		if (error) {
+			response.status(500).send(error.message);
+		} else if (redirectLocation) {
+			response.redirect(302, redirectLocation.pathname + redirectLocation.search);
+		} else if (renderProps) {
+			renderRoute(response, renderProps);
+		} else {
+			response.status(404).send('Not found');
+		}
 	});
 });
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
 	console.log('Express app listening on port 3000');
 });
